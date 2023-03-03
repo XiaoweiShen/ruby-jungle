c17ba65442f6d68d3953cb4e7e3c538619b4b638
XiaoweiShen <snhiui@gmail.com>
2023-03-03 11:33:57 -0800

    user auth done

--- db/migrate/20230303160941_create_users.rb
+++ db/migrate/20230303160941_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :firstname
       t.string :lastname
       t.string :email
-      t.string :passsword_digest
+      t.string :password_digest
 
       t.timestamps
     end
